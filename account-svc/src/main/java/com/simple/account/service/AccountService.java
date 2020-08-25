475814bd7f35cb59e657d20df72a48158283c5a5
windwithlife@hotmail.com <windwithlife@hotmail.com>
2020-08-26 02:10:13 +0800

    update

--- account-svc/src/main/java/com/simple/account/service/AccountService.java
+++ account-svc/src/main/java/com/simple/account/service/AccountService.java
@@ -28,8 +28,8 @@ import com.simple.common.crypto.Sign;
 import com.simple.common.env.EnvConfig;
 import com.simple.common.error.ServiceException;
 import com.simple.common.utils.Helper;
-import com.simple.mail.client.MailClient;
-import com.simple.mail.dto.EmailRequest;
+//import com.simple.mail.client.MailClient;
+//import com.simple.mail.dto.EmailRequest;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -54,7 +54,7 @@ public class AccountService {
 
     private final EnvConfig envConfig;
 
-    private final MailClient mailClient;
+    //private final MailClient mailClient;
 
     private final ServiceHelper serviceHelper;
 
@@ -336,7 +336,7 @@ public class AccountService {
         }
 
         // Send verification email
-        this.sendEmail(account.getId(), email, account.getName(), subject, tmpl, activate);
+        //this.sendEmail(account.getId(), email, account.getName(), subject, tmpl, activate);
     }
 
     // requestEmailChange sends an email to a user with a confirm email link
@@ -347,7 +347,7 @@ public class AccountService {
         }
 
         String subject = "Confirm Your New Email Address";
-        this.sendEmail(account.getId(), email, account.getName(), subject, AccountConstant.CONFIRM_EMAIL_TMPL, true);
+        //this.sendEmail(account.getId(), email, account.getName(), subject, AccountConstant.CONFIRM_EMAIL_TMPL, true);
     }
 
     // ChangeEmail sets an account to active and updates its email. It is
@@ -374,60 +374,60 @@ public class AccountService {
         this.trackEventWithAuthCheck("email_updated");
     }
 
-    void sendEmail(String userId, String email, String name, String subject, String template, boolean activateOrConfirm) {
-        String token = null;
-        try {
-            token = Sign.generateEmailConfirmationToken(userId, email, appProps.getSigningSecret());
-        } catch(Exception ex) {
-            String errMsg = "Could not create token";
-            serviceHelper.handleException(logger, ex, errMsg);
-            throw new ServiceException(errMsg, ex);
-        }
-
-        String pathFormat = "/activate/%s";
-        if (!activateOrConfirm) {
-            pathFormat = "/reset/%s";
-        }
-        String path = String.format(pathFormat, token);
-        URI link = null;
-        try {
-            link = new URI("http", "www." + envConfig.getExternalApex(), path, null);
-        } catch (URISyntaxException ex) {
-            String errMsg = "Could not create activation url";
-            if (!activateOrConfirm) {
-                errMsg = "Could not create reset url";
-            }
-            serviceHelper.handleException(logger, ex, errMsg);
-            throw new ServiceException(errMsg, ex);
-        }
-
-        String htmlBody = null;
-        if (activateOrConfirm) { // active or confirm
-            htmlBody = String.format(template, name, link.toString(), link.toString(), link.toString());
-        } else { // reset
-            htmlBody = String.format(template, link.toString(), link.toString());
-        }
-
-        EmailRequest emailRequest = EmailRequest.builder()
-                .to(email)
-                .name(name)
-                .subject(subject)
-                .htmlBody(htmlBody)
-                .build();
-
-        BaseResponse baseResponse = null;
-        try {
-            baseResponse = mailClient.send(emailRequest);
-        } catch (Exception ex) {
-            String errMsg = "Unable to send email";
-            serviceHelper.handleException(logger, ex, errMsg);
-            throw new ServiceException(errMsg, ex);
-        }
-        if (!baseResponse.isSuccess()) {
-            serviceHelper.handleError(logger, baseResponse.getMessage());
-            throw new ServiceException(baseResponse.getMessage());
-        }
-    }
+//    void sendEmail(String userId, String email, String name, String subject, String template, boolean activateOrConfirm) {
+//        String token = null;
+//        try {
+//            token = Sign.generateEmailConfirmationToken(userId, email, appProps.getSigningSecret());
+//        } catch(Exception ex) {
+//            String errMsg = "Could not create token";
+//            serviceHelper.handleException(logger, ex, errMsg);
+//            throw new ServiceException(errMsg, ex);
+//        }
+//
+//        String pathFormat = "/activate/%s";
+//        if (!activateOrConfirm) {
+//            pathFormat = "/reset/%s";
+//        }
+//        String path = String.format(pathFormat, token);
+//        URI link = null;
+//        try {
+//            link = new URI("http", "www." + envConfig.getExternalApex(), path, null);
+//        } catch (URISyntaxException ex) {
+//            String errMsg = "Could not create activation url";
+//            if (!activateOrConfirm) {
+//                errMsg = "Could not create reset url";
+//            }
+//            serviceHelper.handleException(logger, ex, errMsg);
+//            throw new ServiceException(errMsg, ex);
+//        }
+//
+//        String htmlBody = null;
+//        if (activateOrConfirm) { // active or confirm
+//            htmlBody = String.format(template, name, link.toString(), link.toString(), link.toString());
+//        } else { // reset
+//            htmlBody = String.format(template, link.toString(), link.toString());
+//        }
+//
+//        EmailRequest emailRequest = EmailRequest.builder()
+//                .to(email)
+//                .name(name)
+//                .subject(subject)
+//                .htmlBody(htmlBody)
+//                .build();
+//
+//        BaseResponse baseResponse = null;
+//        try {
+//            baseResponse = mailClient.send(emailRequest);
+//        } catch (Exception ex) {
+//            String errMsg = "Unable to send email";
+//            serviceHelper.handleException(logger, ex, errMsg);
+//            throw new ServiceException(errMsg, ex);
+//        }
+//        if (!baseResponse.isSuccess()) {
+//            serviceHelper.handleError(logger, baseResponse.getMessage());
+//            throw new ServiceException(baseResponse.getMessage());
+//        }
+//    }
 
     public void trackEvent(String userId, String eventName) {
         serviceHelper.trackEventAsync(userId, eventName);
